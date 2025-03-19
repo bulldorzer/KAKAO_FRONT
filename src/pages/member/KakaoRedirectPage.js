e25433480f4e979d9d3614e74ecc162c9373cc51
Bulldorzer <buad452@gmail.com>
2025-03-19 16:00:07 +0900

    인가코드를 받아 승인토큰을받고 받은 코드로 서버API에 전달

--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -1,14 +1,19 @@
-import { useSearchParams } from "react-router-dom";
-import { getAccessToken } from "../../api/kakaoApi";
 import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { getAccessToken, getMemberWithAccessToken } from "../../api/kakaoApi";
 
 const KakaoRedirectPage = () => {
     const [searchParams] = useSearchParams();
     const authCode = searchParams.get("code"); // 인가코드 추출
 
     useEffect(() => {
-        getAccessToken(authCode).then(data => {
-            console.log(data);
+        getAccessToken(authCode).then(accessToken => {
+            console.log(accessToken);
+
+            getMemberWithAccessToken(accessToken).then(memberInfo =>{
+                console.log("----------------------");
+                console.log(memberInfo);
+            })
         })
     }, [authCode]) // 인가코드가 변경 될 때마다, 새로운 access token을 발급 받음
 
